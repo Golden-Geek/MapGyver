2d515bd57066937450c298875f8a660577c16895
Ben Kuper <bkuperberg@gmail.com>
2023-12-20 19:52:44 +0100

    packagers

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "RuleMaPool";
-    const char* const  companyName    = "";
+    const char* const  companyName    = "GoldenGeek";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
